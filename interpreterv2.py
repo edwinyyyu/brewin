b1a173439925bacb8f741e677e3afe97a36b4c3d
Edwin Yu <edwinyyyu@gmail.com>
2023-10-26 20:05:32 -0700

    Implement bool, nil, and more operations

---
+++ interpreterv2.py
@@ -0,0 +1,197 @@
+from brewparse import parse_program
+from element import Element
+from intbase import InterpreterBase, ErrorType
+
+
+class Interpreter(InterpreterBase):
+    builtin_functions = {"print", "inputi", "inputs"}
+
+    def __init__(self, console_output=True, inp=None, trace_output=False):
+        super().__init__(console_output, inp)
+        self.trace_output = trace_output
+
+        self.variables = {}
+
+    def run(self, program):
+        program_node = parse_program(program)
+
+        functions = program_node.get("functions")
+        self.function_defs = {function.get("name"): function for function in functions}
+
+        main_function = Element("fcall", name="main", args=[])
+        self.run_function(main_function)
+
+    def run_function(self, function):
+        name = function.get("name")
+        args = function.get("args")
+
+        if name in self.function_defs:
+            function_def = self.function_defs[name]
+            for statement in function_def.get("statements"):
+                self.run_statement(statement)
+        elif name in self.builtin_functions:
+            return self.run_builtin(function)
+        else:
+            self.error(ErrorType.NAME_ERROR, f"No {name}() function was found")
+
+    def run_builtin(self, function):
+        name = function.get("name")
+        args = function.get("args")
+
+        match name:
+            case "print":
+                self.output("".join(self.fmt(arg) for arg in args))
+                return Element("nil")
+            case "inputi":
+                if len(args) == 1:
+                    self.output(self.evaluate_expression(args[0]).get("val"))
+                elif len(args) > 1:
+                    self.error(
+                        ErrorType.NAME_ERROR,
+                        "No inputi() function found that takes > 1 parameter",
+                    )
+                return Element("int", val=int(self.get_input()))
+            case "inputs":
+                if len(args) == 1:
+                    self.output(self.evaluate_expression(args[0]).get("val"))
+                elif len(args) > 1:
+                    self.error(
+                        ErrorType.NAME_ERROR,
+                        "No inputs() function found that takes > 1 parameter",
+                    )
+                return Element("string", val=self.get_input())
+
+    def run_statement(self, statement):
+        match statement.elem_type:
+            case "=":
+                self.run_assignment(statement)
+            case "fcall":
+                self.run_function(statement)
+
+    def run_assignment(self, assignment):
+        name = assignment.get("name")
+        expression = assignment.get("expression")
+        self.variables[name] = self.evaluate_expression(expression)
+
+    def evaluate_expression(self, expression):
+        match expression.elem_type:
+            case "neg" | "!":
+                return self.evaluate_unary_operation(expression)
+            case "+" | "-" | "*" | "/" | "==" | "!=" | "<" | ">" | "<=" | ">=" | "||" | "&&":
+                return self.evaluate_binary_operation(expression)
+            case "fcall":
+                return self.run_function(expression)
+            case "var":
+                name = expression.get("name")
+                if name in self.variables:
+                    return self.variables[name]
+                else:
+                    self.error(
+                        ErrorType.NAME_ERROR, f"Variable {name} has not been defined"
+                    )
+            case "int" | "string" | "bool" | "nil":
+                return expression
+
+    def evaluate_unary_operation(self, operation):
+        op1 = self.evaluate_expression(operation.get("op1"))
+
+        try:
+            match operation.elem_type:
+                case "neg":
+                    if op1.elem_type == "int":
+                        return Element("int", val=-op1.get("val"))
+                    else:
+                        raise TypeError
+                case "!":
+                    if op1.elem_type == "bool":
+                        return Element("bool", val=not op1.get("val"))
+                    else:
+                        raise TypeError
+        except:
+            self.error(
+                ErrorType.TYPE_ERROR,
+                f"Incompatible type for operation {operation.elem_type}: {op1.elem_type}",
+            )
+
+    def evaluate_binary_operation(self, operation):
+        # Strict evaluation
+        op1 = self.evaluate_expression(operation.get("op1"))
+        op2 = self.evaluate_expression(operation.get("op2"))
+
+        try:
+            match operation.elem_type:
+                case "+":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("int", val=op1.get("val") + op2.get("val"))
+                    elif op1.elem_type == op2.elem_type == "string":
+                        return Element("string", val=op1.get("val") + op2.get("val"))
+                    else:
+                        raise TypeError
+                case "-":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("int", val=op1.get("val") - op2.get("val"))
+                    else:
+                        raise TypeError
+                case "*":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("int", val=op1.get("val") * op2.get("val"))
+                    else:
+                        raise TypeError
+                case "/":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("int", val=op1.get("val") // op2.get("val"))
+                    else:
+                        raise TypeError
+                case "==":
+                    if op1.elem_type != op2.elem_type:
+                        return Element("bool", val=False)
+                    else:
+                        return Element("bool", val=op1.get("val") == op2.get("val"))
+                case "!=":
+                    if op1.elem_type != op2.elem_type:
+                        return Element("bool", val=True)
+                    else:
+                        return Element("bool", val=op1.get("val") != op2.get("val"))
+                case "<":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("bool", val=op1.get("val") < op2.get("val"))
+                    else:
+                        raise TypeError
+                case ">":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("bool", val=op1.get("val") > op2.get("val"))
+                    else:
+                        raise TypeError
+                case "<=":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("bool", val=op1.get("val") <= op2.get("val"))
+                    else:
+                        raise TypeError
+                case ">=":
+                    if op1.elem_type == op2.elem_type == "int":
+                        return Element("bool", val=op1.get("val") >= op2.get("val"))
+                    else:
+                        raise TypeError
+                case "||":
+                    if op1.elem_type == op2.elem_type == "bool":
+                        return Element("bool", val=op1.get("val") or op2.get("val"))
+                    else:
+                        raise TypeError
+                case "&&":
+                    if op1.elem_type == op2.elem_type == "bool":
+                        return Element("bool", val=op1.get("val") and op2.get("val"))
+                    else:
+                        raise TypeError
+        except TypeError:
+            self.error(
+                ErrorType.TYPE_ERROR,
+                f"Incompatible types for operation {operation.elem_type}: {op1.elem_type} and {op2.elem_type}",
+            )
+
+    def fmt(self, expression):
+        element = self.evaluate_expression(expression)
+        match element.elem_type:
+            case "int" | "string":
+                return str(element.get("val"))
+            case "bool":
+                return str(element.get("val")).lower()
